d18bf9d8cbe82aa6337d445cd57266cc0556bafa
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-11-20 11:34:56 -0200

    Update control.sh

--- control.sh
+++ control.sh
@@ -4,22 +4,34 @@ echo "START SCRIPT"
 
 _build()
 {
-  echo "build"
+  echo "************************************build"
+  sudo docker build  -t="tserv-db-img" -f ./Dockerfile .
+  sudo docker tag tserv-db-img andersonalmeidax0/tserv-db-img:v1.0.0
+  sudo docker push  andersonalmeidax0/tserv-db-img:v1.0.0
 }
 
 _start()
 {
-  echo "start"
+  echo "start*****************************"
+  sudo docker run -d -p 9999:9999 -v /home/anderson/data/tserv:/data --name tserv-app-cnr  tserv-app-img
 }
 
 _stoprm()
 {
-  echo "stoprm"
+  echo "stoprm****************************"
+  sudo docker stop  tserv-app-cnr
+  sudo docker rm  tserv-app-cnr
+
 }
 
 _test()
 {
-   echo "test"
+  sudo docker logs tserv-app-cnr
+  echo "-----------------"
+  response=$(curl -X GET localhost:9999/health);
+  echo "-----------------"
+  if [ "${response}" == "APPSERV OK" ]; then echo "SUCESS!!!!!!!"; fi;
+  echo "-----------------"
 }
 
 while [ "$1" != "" ]; do
